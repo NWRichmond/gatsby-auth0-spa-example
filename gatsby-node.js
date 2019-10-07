36b577a4f931820c91ceb101201e8be138d1b6a2
Nick Richmond <nwrichmond@turnt.tech>
2019-10-07 07:26:59 -0400

    Set up client-only routes

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,19 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const clientRoutes = ['account']; // add all client-only routes to this array
 
-// You can delete this file if you're not using it
+exports.onCreatePage = async ({ page, actions }) => {
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  const { createPage } = actions;
+  for (const route of clientRoutes) {
+    const regexRoute = new RegExp(`/${route}`);
+    const path = `/${route}/*`;
+    if (page.path.match(regexRoute)) {
+      page.matchPath = path;
+      // Update the page.
+      createPage(page);
+    }
+  }
+};
