bdee00fb0c1ad5d79831c19de59f04262620ba86
Nick Richmond <nwrichmond@turnt.tech>
2019-10-07 23:50:09 -0400

    Use wrapRootElement for browser & ssr Auth0 config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,32 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Auth0Provider } from './src/components/auth/auth0-wrapper';
 
-// You can delete this file if you're not using it
+const onRedirectCallback = appState => {
+  // route the user to the right place after login
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+export const wrapRootElement = ({ element }) => (
+  <Auth0Provider
+    domain={process.env.AUTH0_DOMAIN}
+    client_id={process.env.AUTH0_CLIENT_ID}
+    redirect_uri={process.env.AUTH0_CALLBACK_URL}
+    onRedirectCallback={onRedirectCallback}
+  >
+    {element}
+  </Auth0Provider>
+);
+
+wrapRootElement.propTypes = {
+  element: PropTypes.element.isRequired,
+};
