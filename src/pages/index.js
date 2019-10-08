fb94f794cfae0d1d3df924d8dce290a1998fbca0
Nick Richmond <nwrichmond@turnt.tech>
2019-10-07 23:58:05 -0400

    Modify content in landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,19 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { Navigation } from '../components/nav-bar';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Home</h1>
+    <Navigation />
+    <p>
+      Check out the private routes (My Account, Settings, and Billing - shown in{' '}
+      <span style={{ color: `green` }}>green</span>) to test the{' '}
+      <code>auth0-spa-js</code> SDK with Gatsby!
+    </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
